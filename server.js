3950ea485382f031e10b2bedb2b8155d26a55ab9
Wanderlei <whande1@gmail.com>
2022-10-22 12:41:54 -0300

    Gazin Frontend

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { resolve } = require('path')
+const app = express
+
+
+app.use('/' ,
+    express.static(
+        resolve(
+            __dirname,
+            './dist'
+        )
+    )
+)
+
+app.listen(process.env.PORT || 3000, (app) => {
+    if(err) {
+        return console.log(err)
+
+        console.log('tudo funcionando certinho ')
+    }})
